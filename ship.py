e24ff50eb422200b1418cb4c9e4085153829e033
leooog <leooog@gmail.com>
2024-09-08 22:16:40 +0800

    buttle

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship():
     def __init__(self,ai_game):
         """初始化飞船，并且设置其初始位置"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #加载飞船图像，并获取其外接矩形
@@ -13,14 +14,23 @@ class Ship():
 
         #对于每艘新飞船，都将起放在屏幕底部的中央。
         self.rect.midbottom = self.screen_rect.midbottom
+
+        self.x = float(self.rect.x)
         
         #移动标志
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """根据移动标志调整飞船的位置"""
-        if self.moving_right:
-            self.rect.x += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
+        #根据self.x 更新rect对象
+
             
 
     def blitme(self):
